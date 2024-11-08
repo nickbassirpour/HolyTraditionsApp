54cf2d35b0d828766fa924a383ee6b12d9a48f62
nickbassirpour <nickbassirpour@gmail.com>
2024-11-07 20:32:26 -0800

    First Commit

---
+++ WebScraper/Models/ArticleModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebScraper.Models
+{
+    internal class ArticleModel
+    {
+        public string Title { get; set; }
+        public string Series {  get; set; }
+        public string Author { get; set; }
+        public string Topic { get; set; }
+        public string 
+    }
+}
---
+++ WebScraper/Program.cs
@@ -0,0 +1,58 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.RegularExpressions;
+
+string htmlContent = @"https://traditioninaction.org/HotTopics/f240_Dialogue_143.htm";
+
+HtmlWeb web = new HtmlWeb()
+{
+    OverrideEncoding = Encoding.UTF8,
+};
+
+var htmlDoc = web.Load(htmlContent);
+
+var topic = htmlDoc.DocumentNode.Descendants().FirstOrDefault(node => node.Id == "topicHeader" || node.Element("h3") != null);
+
+var series = htmlDoc.DocumentNode.SelectSingleNode("//*[@class='GreenSeries']");
+
+var title = htmlDoc.DocumentNode.Descendants().FirstOrDefault(node => node.Name == "h1" || node.Name == "h4");
+
+var author = htmlDoc.DocumentNode.SelectSingleNode("//*[@class='author']");
+
+var date = htmlDoc.DocumentNode.SelectSingleNode("//*[@id='posted' or @id='sitation']");
+
+var footNotes = htmlDoc.DocumentNode.SelectSingleNode("//*[@id='footnotes']");
+if (footNotes != null)
+{
+    var liNodes = footNotes.Descendants("li");
+
+    foreach (var liNode in liNodes)
+    {
+        string liNodeText = liNode.InnerHtml;
+
+        liNodeText.Replace("</li>", "").Replace("<li>", "");
+
+        var matches = Regex.Split(liNodeText, "(<em>|</em>)");
+
+        for (int i = 0; i < matches.Length; i++)
+        {
+            if (matches[i] == "<em>" || matches[i] == "</em>")
+            {
+                matches[i] = null;
+            }
+        }
+
+    }
+}
+
+
+
+Console.WriteLine(topic.InnerText);
+Console.WriteLine(series.InnerText.Trim());
+Console.WriteLine(title.InnerText);
+Console.WriteLine(author.InnerText);
+Console.WriteLine(date.InnerText);
+Console.WriteLine(footNotes.InnerHtml);
+
