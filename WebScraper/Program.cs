d3d8563e391b5f7bb249d31d7b99cb40c5768167
nickbassirpour <nickbassirpour@gmail.com>
2024-11-14 22:04:52 -0800

    Checking topic

--- WebScraper/Program.cs
+++ WebScraper/Program.cs
@@ -11,7 +11,10 @@ void Scrape(string url)
 {
     WebScraperService webScraper = new WebScraperService(url);
 
-    webScraper.GetBody();
+    webScraper.GetFootnotes();
+    webScraper.GetTitle();
+    string topic = webScraper.GetTopic();
+    Console.WriteLine(topic);
 }
 
 Scrape("https://traditioninaction.org/bev/298bev11_08_2024.htm");
