8d9ec3c12b7d97238496f954e60e78d442ef2444
nickbassirpour <nickbassirpour@gmail.com>
2024-11-28 10:14:24 -0800

    Adding more to stored procedure

--- TIABackendDB/dbo/Stored Procedures/Articles_AddNewArticle.sql
+++ TIABackendDB/dbo/Stored Procedures/Articles_AddNewArticle.sql
@@ -1,8 +1,16 @@
 ﻿CREATE PROCEDURE [dbo].[Articles_AddNewArticle]
 	@title varchar(200),
 	@url varchar(300),
+	@category varchar(100),
 	@description varchar(300),
 	@subcategory varchar(100),
+	@series varchar(200),
+	@seriesNumber varchar(20),
+	@author varchar(200),
+	@bodyHtml varchar(MAX),
+	@bodyInnerText varchar(MAX),
+	@date date,
+	@relatedArticles varchar(MAX)
 	
 AS
 	SELECT @param1, @param2
