c213ef4a704f14909080ea5d177c01b8c215a443
nickbassirpour <nickbassirpour@gmail.com>
2025-01-24 20:48:05 -0800

    Adding result pattern

--- TIAArticleAppAPI/Validation/ValidationFailed.cs
+++ TIAArticleAppAPI/Validation/ValidationFailed.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace WebScraper.Validation
+namespace TIAArticleAppAPI.Validation
 {
     public record ValidationFailed(string errorMessage)
     {
