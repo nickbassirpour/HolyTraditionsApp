c2c189489ad6865e6a5f328df2b2afb9aea7b5ed
nickbassirpour <nickbassirpour@gmail.com>
2025-02-01 20:52:02 -0800

    Can't figure out data access

--- WebScraper/Program.cs
+++ WebScraper/Program.cs
@@ -1,76 +1,88 @@
-﻿using System.Text.Json;
+﻿using System;
+using System.Text.Json;
+using TIAArticleAppAPI.Data;
 using TIAArticleAppAPI.Models;
 using TIAArticleAppAPI.Services;
+using WebScraper.Helpers;
 using WebScraper.Services;
 
-string apiBaseUrl = "http://localhost:5223/add_new_article";
-async void ScrapeList(string url)
-{
-    ListScraperService articleListScraper = new ListScraperService(url);
-    List<BaseArticleModel> articlesFromList = articleListScraper.ScrapeArticles();
-    if (articlesFromList != null)
-    {
-        List<ArticleModel> scrapedArticles = new List<ArticleModel>();
-        List<BaseArticleModel> notScrapedArticles = new List<BaseArticleModel>();
-        foreach (BaseArticleModel article in articlesFromList)
-        {
-            if (article.Url.EndsWith(".pdf") || article.Url.Contains("tiabk") || article.Url.EndsWith("pps") || article.Url.EndsWith("mp4"))
-            {
-                continue;
-            }
-            ArticleModel scrapedArticle = Scrape(article);
-            if (scrapedArticle != null)
-            {
-                scrapedArticles.Add(scrapedArticle);
-            }
-            else
-            {
-                notScrapedArticles.Add(article);
-            }
-        }
-        Console.WriteLine();
-        Console.WriteLine("Article List Count: " + articlesFromList.Count());
-        Console.WriteLine("Article Scrape Count: " + scrapedArticles.Count());
-        Console.WriteLine();
-        foreach (BaseArticleModel notScrapedArticle in notScrapedArticles)
-        {
-            Console.WriteLine(notScrapedArticle.Url);
-            Console.WriteLine(notScrapedArticle.Title);
-            Console.WriteLine(notScrapedArticle.Description);
-            Console.WriteLine();
-        }
-    }
 
-}
-ArticleModel? Scrape(BaseArticleModel baseArticle)
-{
-    ArticleScraperService webScraper = new ArticleScraperService(baseArticle, _service);
-    ArticleModel article = webScraper.ScrapeArticle();
-    if (article != null)
-    {
-        Console.WriteLine();
-        Console.WriteLine("Url: " + article.Url);
-        Console.WriteLine("Author: " + article.Author?[0]);
-        Console.WriteLine("Title: " + article.Title);
-        //Console.WriteLine("ThumbnailURL: " + article.ThumbnailURL);
-        //Console.WriteLine("Category: " + article.Category);
-        //Console.WriteLine("SubCategory: " + article.SubCategory);
-        //Console.WriteLine("Date: " + article.Date?.ToString());
-        //Console.WriteLine("Series: " + article.Series);
-        //Console.WriteLine("SeriesNumber: " + article.SeriesNumber);
-        Console.WriteLine("Description: " + baseArticle.Description);
-        //Console.WriteLine("RelatedArticles: " + article.RelatedArticles);
 
-        return article;
-    }
-    return null;
-}
+//async void BeginScrape(string url)
+//{
+
+//    ListScraperService articleListScraper = new ListScraperService(url);
+//}
+//async void ScrapeList(string url)
+//{
+//    ListScraperService articleListScraper = new ListScraperService(url);
+//    List<BaseArticleModel> articlesFromList = articleListScraper.ScrapeArticles();
+//    if (articlesFromList != null)
+//    {
+//        List<ArticleModel> scrapedArticles = new List<ArticleModel>();
+//        List<BaseArticleModel> notScrapedArticles = new List<BaseArticleModel>();
+//        foreach (BaseArticleModel article in articlesFromList)
+//        {
+//            if (article.Url.EndsWith(".pdf") || article.Url.Contains("tiabk") || article.Url.EndsWith("pps") || article.Url.EndsWith("mp4"))
+//            {
+//                continue;
+//            }
+//            ArticleModel scrapedArticle = Scrape(article);
+//            if (scrapedArticle != null)
+//            {
+//                scrapedArticles.Add(scrapedArticle);
+//            }
+//            else
+//            {
+//                notScrapedArticles.Add(article);
+//            }
+//        }
+//        Console.WriteLine();
+//        Console.WriteLine("Article List Count: " + articlesFromList.Count());
+//        Console.WriteLine("Article Scrape Count: " + scrapedArticles.Count());
+//        Console.WriteLine();
+//        foreach (BaseArticleModel notScrapedArticle in notScrapedArticles)
+//        {
+//            Console.WriteLine(notScrapedArticle.Url);
+//            Console.WriteLine(notScrapedArticle.Title);
+//            Console.WriteLine(notScrapedArticle.Description);
+//            Console.WriteLine();
+//        }
+//    }
+
+//}
+//ArticleModel? Scrape(BaseArticleModel baseArticle)
+//{
+//    ArticleScraperService webScraper = new ArticleScraperService(baseArticle);
+//    ArticleModel article = webScraper.ScrapeArticle();
+//    if (article != null)
+//    {
+//        Console.WriteLine();
+//        Console.WriteLine("Url: " + article.Url);
+//        Console.WriteLine("Author: " + article.Author?[0]);
+//        Console.WriteLine("Title: " + article.Title);
+//        //Console.WriteLine("ThumbnailURL: " + article.ThumbnailURL);
+//        //Console.WriteLine("Category: " + article.Category);
+//        //Console.WriteLine("SubCategory: " + article.SubCategory);
+//        //Console.WriteLine("Date: " + article.Date?.ToString());
+//        //Console.WriteLine("Series: " + article.Series);
+//        //Console.WriteLine("SeriesNumber: " + article.SeriesNumber);
+//        Console.WriteLine("Description: " + baseArticle.Description);
+//        //Console.WriteLine("RelatedArticles: " + article.RelatedArticles);
+
+//        return article;
+//    }
+//    return null;
+//}
 
 var jsonLinks = System.IO.File.ReadAllText(@"C:\Users\nickb\Desktop\Code_Projects\TIABackend\WebScraper\Data\testLinks.json");
 var links = JsonSerializer.Deserialize<Dictionary<string, List<string>>>(jsonLinks);
 
 if (links != null)
 {
+    SqlDataAccess db = new SqlDataAccess();
+    IArticleService articleService = new ArticleService(); // Replace with actual implementation
+    ScraperManager scraperManager = new ScraperManager(articleService);
 
     foreach (var category in links)
     {
@@ -86,7 +98,7 @@ if (links != null)
         foreach (string url in urls)
         {
             await Task.Delay(5000);
-            ScrapeList(url);
+            BeginScrape(url);
         }
     }
 }
