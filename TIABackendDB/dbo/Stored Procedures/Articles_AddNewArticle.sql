7e64eee5e02c6b0bcf69f2371851539897b8ebe0
nickbassirpour <nickbassirpour@gmail.com>
2025-02-17 20:40:07 -0800

    Adding database

--- TIABackendDB/dbo/Stored Procedures/Articles_AddNewArticle.sql
+++ TIABackendDB/dbo/Stored Procedures/Articles_AddNewArticle.sql
@@ -1,16 +1,17 @@
 ﻿CREATE PROCEDURE [dbo].[Articles_AddNewArticle]
+	@subcategory varchar(100), --add subcategory to category table
 	@title varchar(200),
 	@url varchar(300),
 	@category varchar(100), --add category to category table
 	@description varchar(300),
-	@subcategory varchar(100), --add subcategory to category table
+	@thumbnailUrl varchar(MAX),
 	@series varchar(200), --add series to series table
 	@seriesNumber varchar(20),
 	@author varchar(200), --add author to author table
-	@bodyHtml varchar(MAX),
-	@bodyInnerText varchar(MAX),
+	@bodyHtml nvarchar(MAX),
+	@bodyInnerText nvarchar(MAX),
 	@date date,
-	@relatedArticles varchar(MAX)
+	@relatedArticles nvarchar(MAX)
 	
 AS
 BEGIN
