eaedb3ff65fe81ed9d35d31af50b4bbdeabe3d57
nickbassirpour <nickbassirpour@gmail.com>
2025-02-19 20:10:48 -0800

    Adding more to SQL

--- TIABackendDB/dbo/Stored Procedures/Articles_AddNewArticle.sql
+++ TIABackendDB/dbo/Stored Procedures/Articles_AddNewArticle.sql
@@ -18,39 +18,60 @@ BEGIN
 	
 	SET NOCOUNT ON;
 
-	BEGIN 
+	DECLARE @CategoryId INT;
+	DECLARE @SubCategoryId INT;
+
+		IF NOT EXISTS (SELECT 1 FROM dbo.Category WHERE [Name] = @Category)
+		BEGIN
+			INSERT INTO 
+				dbo.Category 
+					([Name])
+				VALUES
+					(@category)
+
+			SET @CategoryId = SCOPE_IDENTITY()
+		END
+		ELSE
+		BEGIN
+			SELECT @CategoryId = Id FROM dbo.Category WHERE [Name] = @Category;
+		END
+
+		IF NOT EXISTS (SELECT 1 FROM dbo.SubCategory WHERE [Name] = @SubCategory AND CategoryId = @CategoryId)
+		BEGIN
+			INSERT INTO
+				dbo.SubCategory
+					(
+					[Name],
+					[CategoryId]
+					)
+				VALUES 
+					(
+					@subcategory,
+					@CategoryId
+					)
+				SET @SubCategoryId = SCOPE_IDENTITY()
+		END
+		ELSE
+		BEGIN
+			SELECT @SubCategoryId = Id FROM dbo.SubCategory WHERE [Name] = @SubCategory AND CategoryId = @CategoryId;
+		END
+
+		DECLARE @AuthorIds TABLE (Id INT);
 
 		INSERT INTO
 			dbo.Author
 				(
 				[NAME]
 				)
+			OUTPUT INSERTED.Id INTO @AuthorIds
 			SELECT 
 				a.Name
 			FROM
 				@Authors a
 				WHERE NOT EXISTS (SELECT 1 from dbo.Authors WHERE Name = a.Name)
-		SET @AuthorId = SCOPE_IDENTITY()
 
-		INSERT INTO 
-			dbo.Category 
-				([Name])
-			VALUES
-				(@category)
-		SET @CategoryId = SCOPE_IDENTITY()
-
-		INSERT INTO
-			dbo.SubCategory
-				(
-				[Name],
-				[CategoryId]
-				)
-			VALUES 
-				(
-				@subcategory,
-				@CategoryId
-				)
-		SET @SubCategoryId = SCOPE_IDENTITY()
 
 
 RETURN 0
+
+END;
